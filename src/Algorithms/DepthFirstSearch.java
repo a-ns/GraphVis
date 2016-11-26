cbdc3b3ec20f1520f7b22f3d8b4b97fdc0c2e6d6
Mikayla <mtimm100@gmail.com>
2016-11-26 13:12:20 -0600

    DFS in better shape. Test me!

--- src/Algorithms/DepthFirstSearch.java
+++ src/Algorithms/DepthFirstSearch.java
@@ -1,27 +1,36 @@
 package Algorithms;
 
 import Graph.Edge;
+import Graph.*;
 import Graph.Vertex;
+
+import java.util.ArrayList;
 
 public class DepthFirstSearch{
 	private String visitedNodes;
 	private boolean[] visited;
+	private Graph currentState;
+	private Edge[][] adjMatrix;
+	private Edge[][] newMatrix;
+	private ArrayList<Graph> states;
 	
-	public DepthFirstSearch(Edge[][] adjMatrix, int startID, Vertex startNode){
+	public DepthFirstSearch(Graph g, int startID, Vertex startNode){
 		visitedNodes = "";
+		currentState = g;
+		this.adjMatrix = g.toAdjacencyMatrix();
 		visited = new boolean[adjMatrix[0].length]; //number of vertices
-		depthFirstSearch(adjMatrix, startID, startNode);
+		depthFirstSearch(adjMatrix, startNode);
 		
 	}
-	
-	public String depthFirstSearch(Edge[][] adjMatrix, int startID, Vertex startNode){
+	private void depthFirstSearch(Edge[][] adjMatrix, Vertex startNode){
+		int currentNodeIndex = currentState.getVertices().indexOf(startNode);
 		visitedNodes = visitedNodes + startNode.getValue();
-		visited[startID] = true;
+		visited[currentNodeIndex] = true;
 		for(int i = 0; i < adjMatrix[0].length; i++){
-			if(!visited[i] && adjMatrix[startID][i] != 0){
-				depthFirstSearch(adjMatrix, )
-
-						//doesn't work
+			if(!visited[i] && adjMatrix[currentNodeIndex][i] != null){
+				//if node unvisited and there is a path from current node to that node (edge not null)
+				//do dfs from that node to the rest of the nodes it's attached to
+				depthFirstSearch(adjMatrix, currentState.getVertices().get(i));
 			}
 		}
 	}
